3043f956696e4c6cc50667e62428ecea6bdc6453
Jasantiago02 <jesussantiagolu@gmail.com>
2026-04-30 19:23:14 -0400

    Correcciones
    Se han arreglado algunos errores con respecto al async

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/login/login.jsx';
-
+import UserList from './components/users/UserList/userlist.jsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/userlist" element={<Navigate to="/login" />} />
+        <Route path="/userlist" element={<UserList />} />
       </Routes>
     </BrowserRouter>
 
