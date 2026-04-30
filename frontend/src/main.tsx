82b9fa04fccc5cf86413306842e255becaeb62cc
Jasantiago02 <jesussantiagolu@gmail.com>
2026-04-30 17:51:03 -0400

    Update
    Instalacion de react-route-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/login/login.jsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/userlist" element={<Navigate to="/userlist" />} />
+      </Routes>
+    </BrowserRouter>
+
   </StrictMode>,
 )
