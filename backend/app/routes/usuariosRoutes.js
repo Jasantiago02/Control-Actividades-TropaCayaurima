162764db06f42169b42de1fd339c86f228f3d3f1
viludr626 <viancadosreis1204@hotmail.com>
2026-04-30 21:01:48 -0400

    Controladores y rutas - se crearon los controladores para Crear, Buscar, Actualizar e Inhabilitara los usuarios - se crearon las rutas para los controladores creados

--- backend/app/routes/usuariosRoutes.js
+++ backend/app/routes/usuariosRoutes.js
@@ -1,9 +1,26 @@
 import express from 'express';
 // comando para importar funcion del controlador de usuarios
-import {getUsuarios} from "../controllers/usuariosController.js";
+import {
+    getUsuarios,
+    createUsuario,
+    getUsuarioById,
+    updateUsuario,
+    disableUsuario
+} from "../controllers/usuariosController.js";
 // comando para importar funcion para generar rutas de express
 const router = express.Router();
+
 // comando para generar rutas de usuarios
-router.post('/listarUsuarios', getUsuarios);
+//listar todos los usuarios
+router.get('/listarUsuarios', getUsuarios);
+// Crear un nuevo usuario
+router.post('/crear', createUsuario);
+// Obtener un usuario específico por su ID
+router.get('/buscar/:id', getUsuarioById);
+// Actualizar datos de forma parcial (nombre, telefono, etc.)
+router.patch('/actualizar/:id', updateUsuario);
+// Inhabilitar usuario (Borrado lógico cambiando el estado)
+router.put('/inhabilitar/:id', disableUsuario);
+
 //comando para exportar rutas de usuarios
 export default router;
