619af41923fcca1144fddb1395dd7e982415c8b2
viludr626 <viancadosreis1204@hotmail.com>
2026-04-30 21:35:20 -0400

    Se agrego la ruta de registrar al controlador principal de las rutas

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/login/login.jsx';
 import UserList from './components/users/UserList/userlist.jsx';
+import UserRegister from './components/users/UserRegister/userregister.js';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/userlist" element={<UserList />} />
+        <Route path='/registrar' element={<UserRegister/>}/>
       </Routes>
     </BrowserRouter>
 
